241c71e85919242d735c3239cab527d838bbdc29
Lars Mueller <appgurulars@gmx.de>
2026-07-18 01:40:43 +0200

    Fetch libogg, libvorbis via HTTPS

--- scripts/Vorbis.sh
+++ scripts/Vorbis.sh
@@ -3,8 +3,8 @@ ogg_ver=1.3.6
 vorbis_ver=1.3.7
 
 download () {
-	get_tar_archive libogg "http://downloads.xiph.org/releases/ogg/libogg-${ogg_ver}.tar.xz"
-	get_tar_archive libvorbis "http://downloads.xiph.org/releases/vorbis/libvorbis-${ogg_ver}.tar.xz"
+	get_tar_archive libogg "https://ftp.osuosl.org/pub/xiph/releases/ogg/libogg-${ogg_ver}.tar.xz"
+	get_tar_archive libvorbis "https://ftp.osuosl.org/pub/xiph/releases/vorbis/libvorbis-${ogg_ver}.tar.xz"
 
 	# Remove a flag that breaks the x86 build, configure adds it without checking...
 	sed 's|-mno-ieee-fp||g' -i libvorbis/configure
